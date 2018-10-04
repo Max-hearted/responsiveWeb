aa426191818bc37b79d78eeff5e5b09972108bd6
Max-hearted <652567039@qq.com>
2018-10-04 21:22:46 +0800

    no commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const rev = require('gulp-rev');
+const revReplace = require('gulp-rev-replace');
+const useref = require('gulp-useref');
+const filter = require('gulp-filter');
+const uglify = require('gulp-uglify');
+const csso = require('gulp-csso');
+
+gulp.task('default', function () {
+  let jsFiilter = filter('**/*.js', { restore: true });
+  let cssFilter = filter('**/*.css', { restore: true });
+  let indexHtmlFilter = filter(['**/*', '**/index.html'], { restore: true });
+
+  return gulp.src('src/index.html')
+    .pipe(useref())
+    .pipe(jsFiilter)
+    .pipe(uglify())
+    .pipe(jsFiilter.restore)
+    .pipe(cssFilter)
+    .pipe(csso())
+    .pipe(cssFilter.restore)
+    .pipe(indexHtmlFilter)
+    .pipe(rev())
+    .pipe(indexHtmlFilter.restore)
+    .pipe(revReplace())
+    .pipe(gulp.dest('dist'));
+});
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    autoplay: true,
+    loop: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true
+  });
+});
